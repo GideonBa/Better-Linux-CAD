61bf51a7e1f5db5845c082a7718eb8a1e59609f5
GideonBa <baumanngideon@gmail.com>
2026-07-08 22:25:36 +0200

    Add semantic face references and derived workplanes

--- include/blcad/core/datum_plane.hpp
+++ include/blcad/core/datum_plane.hpp
@@ -5,8 +5,30 @@
 #include "blcad/core/spatial.hpp"
 
 #include <string>
+#include <string_view>
 
 namespace blcad {
+
+enum class SemanticFace {
+  Top,
+};
+
+[[nodiscard]] std::string_view to_string(SemanticFace face) noexcept;
+
+class SemanticFaceReference {
+public:
+  [[nodiscard]] static Result<SemanticFaceReference> create(FeatureId source_feature,
+                                                            SemanticFace face);
+
+  [[nodiscard]] const FeatureId& source_feature() const noexcept;
+  [[nodiscard]] SemanticFace face() const noexcept;
+
+private:
+  SemanticFaceReference(FeatureId source_feature, SemanticFace face);
+
+  FeatureId source_feature_;
+  SemanticFace face_;
+};
 
 class DatumPlane {
 public:
@@ -30,6 +52,23 @@ private:
   Vector3 x_axis_;
   Vector3 y_axis_;
   Vector3 normal_;
+};
+
+class DerivedWorkplane {
+public:
+  [[nodiscard]] static Result<DerivedWorkplane>
+  create_on_feature_face(DatumPlaneId id, std::string name, SemanticFaceReference face_reference);
+
+  [[nodiscard]] const DatumPlaneId& id() const noexcept;
+  [[nodiscard]] const std::string& name() const noexcept;
+  [[nodiscard]] const SemanticFaceReference& face_reference() const noexcept;
+
+private:
+  DerivedWorkplane(DatumPlaneId id, std::string name, SemanticFaceReference face_reference);
+
+  DatumPlaneId id_;
+  std::string name_;
+  SemanticFaceReference face_reference_;
 };
 
 } // namespace blcad
