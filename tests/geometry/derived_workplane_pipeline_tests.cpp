9dbb488930f8b4ee97a98c85aa8967c04f28c504
GideonBa <baumanngideon@gmail.com>
2026-07-08 22:43:51 +0200

    Add off-center derived workplane cut test

--- tests/geometry/derived_workplane_pipeline_tests.cpp
+++ tests/geometry/derived_workplane_pipeline_tests.cpp
@@ -3,7 +3,10 @@
 #include "blcad/geometry/rectangle_extrusion_adapter.hpp"
 #include "blcad/geometry/shape_cache.hpp"
 
+#include <catch2/catch_approx.hpp>
 #include <catch2/catch_test_macros.hpp>
+
+#include <numbers>
 
 using namespace blcad;
 using namespace blcad::geometry;
@@ -20,7 +23,7 @@ Parameter make_length_parameter(const char* id, const char* name, double value_m
   return parameter.value();
 }
 
-PartDocument make_top_face_cut_document() {
+PartDocument make_top_face_cut_document(Point2 hole_center = {}) {
   auto document = PartDocument::create(DocumentId("part.top_face_plate"), "TopFacePlate");
   REQUIRE(document);
 
@@ -62,7 +65,7 @@ PartDocument make_top_face_cut_document() {
                                         DatumPlaneId("workplane.base_top"));
   REQUIRE(top_hole_sketch);
   auto circle = CircleProfile::create(ProfileId("profile.top_hole"),
-                                      ParameterId("part.top_hole_diameter"));
+                                      ParameterId("part.top_hole_diameter"), hole_center);
   REQUIRE(circle);
   REQUIRE(top_hole_sketch.value().add_profile(circle.value()));
   REQUIRE(document.value().add_sketch(top_hole_sketch.value()));
@@ -107,4 +110,28 @@ TEST_CASE("MVP-2 seed recomputes a cut from a sketch on a derived top-face workp
   const ShapeSummary final_summary = inspector.summarize(*cache.final_shape());
   CHECK(final_summary.solid_count == 1);
   CHECK(final_summary.volume_mm3 < base_volume);
+}
+
+TEST_CASE("Derived top-face workplane cut follows an off-center sketch point",
+          "[geometry][workplane]") {
+  const PartDocument document = make_top_face_cut_document(Point2{25.0, -10.0});
+  ShapeCache cache = make_shape_cache();
+  const GeometryRecomputeExecutor executor;
+
+  const auto summary = executor.execute_document(document, cache);
+
+  REQUIRE(summary);
+  CHECK(summary.value().executed_feature_count == 2);
+
+  const RectangleExtrusionAdapter inspector;
+  const GeometryShape* base_shape = cache.find_feature_shape(FeatureId("feature.base_extrude"));
+  REQUIRE(base_shape != nullptr);
+  const double base_volume = inspector.summarize(*base_shape).volume_mm3;
+
+  REQUIRE(cache.final_shape() != nullptr);
+  const ShapeSummary final_summary = inspector.summarize(*cache.final_shape());
+  REQUIRE(final_summary.solid_count == 1);
+
+  const double expected_removed_volume = std::numbers::pi * 10.0 * 10.0 * 8.0;
+  CHECK(final_summary.volume_mm3 == Catch::Approx(base_volume - expected_removed_volume).margin(1.0));
 }
