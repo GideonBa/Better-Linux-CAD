3288de266185d20ab14a6a0c4a17c4c80569b9cc
GideonBa <baumanngideon@gmail.com>
2026-07-09 18:15:30 +0200

    Add construction point resolver interface

---
+++ include/blcad/geometry/construction_point_resolver.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "blcad/core/construction_geometry.hpp"
+#include "blcad/core/part_document.hpp"
+#include "blcad/core/result.hpp"
+#include "blcad/core/spatial.hpp"
+
+namespace blcad::geometry {
+
+struct ResolvedConstructionPoint {
+  ConstructionPointId id;
+  Point3 position;
+};
+
+class ConstructionPointResolver {
+public:
+  [[nodiscard]] Result<ResolvedConstructionPoint>
+  resolve(const PartDocument& document, ConstructionPointId point_id) const;
+};
+
+} // namespace blcad::geometry
