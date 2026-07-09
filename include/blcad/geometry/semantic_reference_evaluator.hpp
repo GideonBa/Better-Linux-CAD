9b6c4c63b13fd2de98b7abe21b46012bda66ec7b
GideonBa <baumanngideon@gmail.com>
2026-07-09 18:06:38 +0200

    Add semantic reference evaluator interface

---
+++ include/blcad/geometry/semantic_reference_evaluator.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "blcad/core/datum_plane.hpp"
+#include "blcad/core/part_document.hpp"
+#include "blcad/core/result.hpp"
+#include "blcad/core/spatial.hpp"
+
+namespace blcad::geometry {
+
+struct ResolvedSemanticEdge {
+  SemanticEdgeReference reference;
+  Point3 start;
+  Point3 end;
+};
+
+struct ResolvedSemanticVertex {
+  SemanticVertexReference reference;
+  Point3 position;
+};
+
+class SemanticReferenceEvaluator {
+public:
+  [[nodiscard]] Result<ResolvedSemanticEdge>
+  resolve_edge(const PartDocument& document, SemanticEdgeReference reference) const;
+
+  [[nodiscard]] Result<ResolvedSemanticVertex>
+  resolve_vertex(const PartDocument& document, SemanticVertexReference reference) const;
+};
+
+} // namespace blcad::geometry
