79c677a7b44e5c16b086f2e07b4bea8e468bf84c
GideonBa <baumanngideon@gmail.com>
2026-07-09 18:59:07 +0200

    Add sketch reference projector interface

---
+++ include/blcad/geometry/sketch_reference_projector.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "blcad/core/part_document.hpp"
+#include "blcad/core/result.hpp"
+#include "blcad/core/spatial.hpp"
+#include "blcad/geometry/workplane_resolver.hpp"
+
+namespace blcad::geometry {
+
+struct ResolvedSketchPointReference {
+  SketchEntityId id;
+  Point2 position;
+};
+
+struct ResolvedSketchLineReference {
+  SketchEntityId id;
+  Point2 point;
+  Vector2 direction;
+};
+
+class SketchReferenceProjector {
+public:
+  [[nodiscard]] Result<ResolvedSketchPointReference>
+  resolve_point(const PartDocument& document, const Sketch& sketch,
+                const ProjectedSketchPoint& reference) const;
+
+  [[nodiscard]] Result<ResolvedSketchLineReference>
+  resolve_line(const PartDocument& document, const Sketch& sketch,
+               const ProjectedSketchLine& reference) const;
+};
+
+} // namespace blcad::geometry
