21a1167720bf7f546b495c957bcfbe37cfd561e6
GideonBa <baumanngideon@gmail.com>
2026-07-10 17:31:54 +0200

    Add sketch repair command label API

---
+++ include/blcad/core/sketch_repair_command_labels.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "blcad/core/sketch_repair_suggestions.hpp"
+#include "blcad/core/sketch_repair_undo_stack_summary.hpp"
+
+#include <string>
+
+namespace blcad {
+
+class SketchRepairCommandLabel {
+public:
+  SketchRepairCommandLabel(std::string title, std::string description);
+
+  [[nodiscard]] const std::string& title() const noexcept;
+  [[nodiscard]] const std::string& description() const noexcept;
+
+private:
+  std::string title_;
+  std::string description_;
+};
+
+class SketchRepairCommandLabeler {
+public:
+  [[nodiscard]] SketchRepairCommandLabel label_for(SketchRepairSuggestionAction action) const;
+  [[nodiscard]] SketchRepairCommandLabel label_for(
+      const SketchRepairUndoStackSummaryEntry& entry) const;
+};
+
+} // namespace blcad
