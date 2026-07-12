b51a9b756a6bdf393692fe01ebd41faa6644db90
GideonBa <baumanngideon@gmail.com>
2026-07-12 13:01:02 +0200

    Add shared Revolute residual construction contract

---
+++ src/geometry/assembly_revolute_joint_residual.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "blcad/geometry/assembly_revolute_joint_equation_builder.hpp"
+
+namespace blcad::geometry::detail {
+
+[[nodiscard]] RevoluteJointResidualDescriptor build_revolute_joint_residual(
+    const AssemblySpaceAxisDescriptor& axis_a,
+    const AssemblySpacePlanarDescriptor& seat_a,
+    const AssemblySpaceAxisDescriptor& axis_b,
+    const AssemblySpacePlanarDescriptor& seat_b,
+    double requested_coordinate_deg) noexcept;
+
+} // namespace blcad::geometry::detail
