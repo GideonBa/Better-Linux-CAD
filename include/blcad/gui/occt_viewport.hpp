059cac3cb11481f55786a9fe242f4b762d126129
GideonBa <baumanngideon@gmail.com>
2026-07-15 13:44:52 +0200

    Block 107: expose viewport Sketch interaction bridge

--- include/blcad/gui/occt_viewport.hpp
+++ include/blcad/gui/occt_viewport.hpp
@@ -1,9 +1,10 @@
 #pragma once
 
 #include "blcad/core/result.hpp"
-#include "blcad/geometry/viewport_scene.hpp"
-#include "blcad/gui/gui_types.hpp"
 #include "blcad/core/spatial.hpp"
+#include "blcad/geometry/viewport_scene.hpp"
+#include "blcad/gui/gui_sketch_interaction.hpp"
+#include "blcad/gui/gui_types.hpp"
 
 #include <QPoint>
 #include <QWidget>
@@ -49,6 +50,10 @@ struct GuiViewportCameraBookmark {
 
 class OcctViewport final : public QWidget {
 public:
+  using SketchPointerCallback =
+      std::function<void(Point2, const GuiSketchSnapResult&, const std::optional<GuiSketchHit>&)>;
+  using SketchSelectionCallback = std::function<void(const std::vector<GuiSelection>&)>;
+
   explicit OcctViewport(QWidget* parent = nullptr);
   ~OcctViewport() override;
 
@@ -63,6 +68,15 @@ public:
   void set_sketch_focus(std::string sketch_id,
                         GuiSketchSurroundingsMode mode = GuiSketchSurroundingsMode::Dim);
   void clear_sketch_focus();
+  [[nodiscard]] Result<std::size_t>
+  set_sketch_interaction(GuiSketchPlaneView plane, GuiSketchInteractionScene scene,
+                         GuiSketchInteractionConfig config = {});
+  void clear_sketch_interaction();
+  void set_sketch_selection_enabled(bool enabled) noexcept;
+  void set_sketch_inference_anchor(std::optional<Point2> anchor) noexcept;
+  void set_sketch_grid_config(GuiSketchGridConfig config);
+  void set_sketch_pointer_callback(SketchPointerCallback callback);
+  void set_sketch_selection_callback(SketchSelectionCallback callback);
   void set_context_menu_callback(std::function<void(QPoint)> callback);
   void fit_all();
 
@@ -80,6 +94,13 @@ public:
   [[nodiscard]] bool sketch_focus_active() const noexcept;
   [[nodiscard]] std::string_view sketch_focus_id() const noexcept;
   [[nodiscard]] GuiSketchSurroundingsMode sketch_surroundings_mode() const noexcept;
+  [[nodiscard]] bool sketch_interaction_active() const noexcept;
+  [[nodiscard]] bool sketch_selection_enabled() const noexcept;
+  [[nodiscard]] const std::vector<GuiSelection>& sketch_selections() const noexcept;
+  [[nodiscard]] const std::optional<GuiSketchSnapResult>& sketch_snap_result() const noexcept;
+  [[nodiscard]] const std::optional<GuiSketchHit>& hovered_sketch_hit() const noexcept;
+  [[nodiscard]] const std::optional<GuiSketchScreenRect>& sketch_box_selection() const noexcept;
+  [[nodiscard]] std::size_t sketch_grid_line_count() const noexcept;
   [[nodiscard]] bool native_viewer_available() const noexcept;
   [[nodiscard]] const std::string& initialization_error() const noexcept;
 
@@ -100,23 +121,38 @@ private:
   void apply_display_mode();
   void apply_selection_filters();
   void apply_sketch_focus();
+  void rebuild_sketch_grid();
+  void update_sketch_pointer(GuiSketchScreenPoint screen_point);
+  void publish_sketch_selection();
   void publish_selection(std::optional<GuiSelection> selection);
   [[nodiscard]] std::optional<GuiSelection> detected_selection() const;
 
   std::unique_ptr<Impl> impl_;
+  std::unique_ptr<GuiSketchInteractionController> sketch_interaction_;
   std::function<void(std::optional<GuiSelection>)> selection_callback_;
+  SketchPointerCallback sketch_pointer_callback_;
+  SketchSelectionCallback sketch_selection_callback_;
   std::function<void(QPoint)> context_menu_callback_;
   std::optional<GuiSelection> selected_semantic_;
+  std::vector<GuiSelection> sketch_selections_;
   std::optional<GuiPlaneCamera> plane_camera_;
+  std::optional<Point2> sketch_inference_anchor_;
+  std::optional<GuiSketchSnapResult> sketch_snap_result_;
+  std::optional<GuiSketchHit> hovered_sketch_hit_;
+  std::optional<GuiSketchScreenRect> sketch_box_selection_;
   GuiViewportDisplayMode display_mode_{GuiViewportDisplayMode::ShadedWithEdges};
   GuiViewportProjection projection_{GuiViewportProjection::Perspective};
   GuiSketchSurroundingsMode sketch_surroundings_mode_{GuiSketchSurroundingsMode::Dim};
   std::uint32_t selection_filter_mask_{0xFFFFFFFFU};
   std::string sketch_focus_id_;
+  QWidget* sketch_overlay_{nullptr};
   QPoint last_mouse_position_;
   QPoint right_press_position_;
+  QPoint sketch_press_position_;
   bool panning_{false};
   bool rotating_{false};
+  bool sketch_box_active_{false};
+  bool sketch_selection_enabled_{true};
 };
 
 } // namespace blcad::gui
