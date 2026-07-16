37fc96aa856bcdce5fb121060afe7fc8a4e5a063
GideonBa <baumanngideon@gmail.com>
2026-07-16 12:59:54 +0200

    Block 114: register redundancy glyph tests

--- tests/gui/gui_test_main.cpp
+++ tests/gui/gui_test_main.cpp
@@ -4,6 +4,7 @@
 #include <catch2/catch_session.hpp>
 
 #include "gui_sketch_constraint_tests.inc"
+#include "gui_sketch_constraint_redundancy_tests.inc"
 
 int main(int argc, char* argv[]) {
   if (qEnvironmentVariableIsEmpty("QT_QPA_PLATFORM")) {
