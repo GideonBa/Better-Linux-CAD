61740bf3c95053ffc35d57ee4392c3d442448da8
GideonBa <baumanngideon@gmail.com>
2026-07-16 13:02:48 +0200

    Block 114: register complete family matrix tests

--- tests/gui/gui_test_main.cpp
+++ tests/gui/gui_test_main.cpp
@@ -5,6 +5,7 @@
 
 #include "gui_sketch_constraint_tests.inc"
 #include "gui_sketch_constraint_redundancy_tests.inc"
+#include "gui_sketch_constraint_family_tests.inc"
 
 int main(int argc, char* argv[]) {
   if (qEnvironmentVariableIsEmpty("QT_QPA_PLATFORM")) {
